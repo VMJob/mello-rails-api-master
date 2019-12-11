512aad837539cc8ab6b468859ca8e78cea05d127
Vincent-Job <vinjob30@gmail.com>
2019-12-11 01:04:03 +0000

    initial commit

---
+++ public/assets/js/user-boards.js
@@ -0,0 +1,31 @@
+const $logoutButton = $('#logout');
+const $newBoardButton = $('#new-board');
+
+let user;
+
+init();
+
+function init() {
+  user = JSON.parse(localStorage.getItem('user'));
+
+  if (!user) {
+    localStorage.clear();
+    location.replace('/');
+    return;
+  }
+
+  $('.welcome h1').text('Welcome ' + user.email + '!');
+}
+
+function handleLogout() {
+  $.ajax({
+    url: '/logout',
+    method: 'DELETE'
+  }).then(function() {
+    localStorage.clear();
+    location.replace('/');
+  });
+}
+
+$logoutButton.on('click', handleLogout);
+$newBoardButton.on('click', MicroModal.show.bind(null, 'create-board'));
