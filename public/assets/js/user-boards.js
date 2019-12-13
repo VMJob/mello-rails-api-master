9329d6adf04ad966b08035a67148b14494d4658e
Vincent-Job <vinjob30@gmail.com>
2019-12-13 01:08:06 +0000

    mello update

--- public/assets/js/user-boards.js
+++ public/assets/js/user-boards.js
@@ -1,5 +1,8 @@
 const $logoutButton = $('#logout');
 const $newBoardButton = $('#new-board');
+const $boardNameInput = $('#board-name');
+const $saveBoardButton = $('#save-board');
+const $boardsContainer = $('.boards');
 
 let user;
 
@@ -15,6 +18,54 @@ function init() {
   }
 
   $('.welcome h1').text('Welcome ' + user.email + '!');
+
+  getUserBoards();
+}
+
+function getUserBoards() {
+  $.ajax({
+    url: `/api/users/${user.id}`,
+    method: 'GET'
+  }).then(function(data) {
+    renderBoards(data.boards);
+  });
+}
+
+function renderBoards(boards) {
+  $boardsContainer.empty();
+
+  let $boardTiles = boards.map(function(board) {
+    let $boardTile = $('<a class="board-tile">')
+      .attr('href', `/boards/${board.id}`)
+      .text(board.name);
+
+    return $boardTile;
+  });
+
+  $boardsContainer.append($boardTiles);
+}
+
+function handleBoardCreate(event) {
+  event.preventDefault();
+
+  let boardName = $boardNameInput.val().trim();
+
+  $boardNameInput.val('');
+
+  if (!boardName) {
+    return;
+  }
+
+  $.ajax({
+    url: '/api/boards',
+    data: {
+      name: boardName
+    },
+    method: 'POST'
+  }).then(function() {
+    getUserBoards();
+    MicroModal.close('create-board');
+  });
 }
 
 function handleLogout() {
@@ -29,3 +80,4 @@ function handleLogout() {
 
 $logoutButton.on('click', handleLogout);
 $newBoardButton.on('click', MicroModal.show.bind(null, 'create-board'));
+$saveBoardButton.on('click', handleBoardCreate);
